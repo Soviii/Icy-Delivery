7e8ac3baf92ec1e5063124e22bc8b0a84306ac44
Soviii <ssonzeu@gmail.com>
2023-04-25 19:39:33 -0700

    implemented initial layout, need to apply functionality and figure out password

--- src/components/AccountDetails/Account.js
+++ src/components/AccountDetails/Account.js
@@ -1,176 +1,137 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Form, Button, InputGroup, FormControl } from "react-bootstrap";
+import Axios from "axios";
 
 const Account = () => {
     const [currentUser, setCurrentUser] = useState();
     const [isLoading, setLoading] = useState(true);
-    const [shippingEmail, setShippingEmail] = useState("");
-    const [shippingFirstName, setShippingFirstName] = useState("");
-    const [shippingLastName, setShippingLastName] = useState("");
-    const [shippingAddress, setShippingAddress] = useState("");
-    const [shippingCity, setShippingCity] = useState("");
-    const [shippingState, setShippingState] = useState("");
-    const [shippingZip, setShippingZip] = useState("");
-    const [shippingCountry, setShippingCountry] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [dateOfBirth, setDateOfBirth] = useState("none");
+    const [passwordMasked, setPasswordMasked] = useState(true);
+    const [showPassword, setShowPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [street, setStreet] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");
+    const [zip, setZip] = useState("");
+    const [country, setCountry] = useState("");
     const [rowsData, setRowsData] = useState([]);
     const [checked, setChecked] = useState(true);
-  
-    const loggedInUserID = localStorage.currentUserID;
-  
+    const [changesMade, setChangesMade] = useState(false);
+
+    let originalEmail = "";
+    let originalPassword = "";
+    let originalDateOfBirth = "";
+    let originalFirstName = "";
+    let originalLastName = "";
+    let originalStreet = "";
+    let originalCity = "";
+    let originalState = "";
+    let originalZip = "";
+    let originalCountry = "";
+
     useEffect(() => {
-      Axios.get(`http://localhost:4000/users/getUser?id=${loggedInUserID}`).then((response) => {
-        setCurrentUser(response.data);
-        setLoading(false);
-      });
-    }, [setCurrentUser]);
-  
-    if (isLoading) {
-      return (
-        <div className='orders'>
-          <h2 style={{ display: 'flex', justifyContent: 'center' }}>Please Log In</h2>
-        </div>
-      );
-    }
-  
-    const handleCheckBoxChange = () => {
-      setChecked(!checked)
-      if (checked) {
-        setShippingAddress(currentUser.data.address.street);
-        setShippingState(currentUser.data.address.state);
-        setShippingZip(currentUser.data.address.zip);
-        setShippingCity(currentUser.data.address.city);
-        setShippingEmail(currentUser.data.email);
-        setShippingFirstName(currentUser.data.firstName);
-        setShippingLastName(currentUser.data.lastName);
-        setShippingCountry(currentUser.data.address.country);
-      }
-      else {
-        setShippingAddress("");
-        setShippingState("");
-        setShippingZip("");
-        setShippingEmail("");
-        setShippingCity("");
-        setShippingFirstName("");
-        setShippingLastName("");
-        setShippingCountry("");
-      }
-    }
-  
-    const addTableRows = () => {
-      const rowsInput = {
-        flavor: '',
-        quantity: ''
-      }
-      setRowsData([...rowsData, rowsInput])
-    }
-  
-    const deleteTableRows = (index) => {
-      const rows = [...rowsData];
-      rows.splice(index, 1);
-      setRowsData(rows);
-    }
-  
-    const handleTableChange = (index, evnt) => {
-      const { name, value } = evnt.target;
-      const rowsInput = [...rowsData];
-      rowsInput[index][name] = value;
-      setRowsData(rowsInput);
-    }
-  
-  
+        const getUserInfo = async () => {
+
+            try {
+                const response = await Axios.get(`http://localhost:4000/users/getUser?id=${localStorage.currentUserID}`);
+
+                console.log(response.data.data);
+            } catch (err) {
+                console.log(err.message);
+            }
+
+
+            
+        }
+
+        getUserInfo();
+        // Fetch current user data from backend
+        // Set currentUser and setLoading when response received
+    }, []);
+
+    const handleShowPassword = () => {
+        setPasswordMasked(!passwordMasked);
+    };
+
+    const handleUpdateAccount = () => {
+        // Send updated user data to backend
+        // Update changesMade state when response received
+    };
+
+    const handleDeleteAccount = () => {
+        // Send delete request to backend
+        // Redirect to login page or show success message when response received
+    };
+
+
     return (
-        <div className='orders'>
-  
-            <div className='child'>
-            <p className='shippingaddressheader'>Shipping Address</p>
-            <Form>
-                <Form.Group size="lg" controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    autoFocus
-                    type="email"
-                    value={shippingEmail}
-                    onChange={(e) => setShippingEmail(e.target.value)}
-                />
-                </Form.Group>
-                <Form.Group size="lg" controlId="firstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control
-                    autoFocus
-                    type="firstName"
-                    value={shippingFirstName}
-                    onChange={(e) => setShippingFirstName(e.target.value)}
-                />
-                </Form.Group>
-                <Form.Group size="lg" controlId="lastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control
-                    autoFocus
-                    type="lastName"
-                    value={shippingLastName}
-                    onChange={(e) => setShippingLastName(e.target.value)}
-                />
-                </Form.Group>
-                <Form.Group size="lg" controlId="address">
-                <Form.Label>Address</Form.Label>
-                <Form.Control
-                    autoFocus
-                    type="address"
-                    value={shippingAddress}
-                    onChange={(e) => setShippingAddress(e.target.value)}
-                />
-                </Form.Group>
-                <Form.Group size="lg" controlId="city">
-                <Form.Label>City</Form.Label>
-                <Form.Control
-                    autoFocus
-                    type="city"
-                    value={shippingCity}
-                    onChange={(e) => setShippingCity(e.target.value)}
-                />
-                </Form.Group>
-                <Form.Group size="lg" controlId="state">
-                <Form.Label>State</Form.Label>
-                <Form.Control
-                    autoFocus
-                    type="state"
-                    value={shippingState}
-                    onChange={(e) => setShippingState(e.target.value)}
-                />
-                </Form.Group>
-                <Form.Group size="lg" controlId="zip">
-                <Form.Label>Address</Form.Label>
-                <Form.Control
-                    autoFocus
-                    type="zip"
-                    value={shippingZip}
-                    onChange={(e) => setShippingZip(e.target.value)}
-                />
-                <Form.Group size="lg" controlId="Country">
-                    <Form.Label>Country</Form.Label>
-                    <Form.Control
-                    autoFocus
-                    type="state"
-                    value={shippingCountry}
-                    onChange={(e) => setShippingCountry(e.target.value)}
-                    />
-                </Form.Group>
-                </Form.Group>
-                <Form.Check
-                type='switch'
-                id='custome-switch'
-                label='Use Default Information'
-                onChange={handleCheckBoxChange}
-                />
-            </Form>
-            <div className="button">
-                <button type="submit" className="btn btn-primary">Checkout</button>
+        <div className="account-details-container">
+            <h2>Account Details</h2>
+            <div className="columns">
+                <div className="column">
+                    <Form>
+                        <Form.Group controlId="formFirstName">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group controlId="formLastName">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group controlId="formDateOfBirth">
+                            <Form.Label>Date of Birth</Form.Label>
+                            <Form.Control type="text" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group controlId="formEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group controlId="formPassword">
+                            <Form.Label>Password</Form.Label>
+                            <InputGroup className="mb-3">
+                                <Button variant="outline-secondary" onClick={handleShowPassword}>UpdatePassword</Button>
+                                {/* <FormControl type={passwordMasked ? "password" : "text"} value={password} onChange={(e) => setPassword(e.target.value)} />
+                                <Button variant="outline-secondary" onClick={handleShowPassword}>{passwordMasked ? "Show" : "Hide"}</Button> */}
+                            </InputGroup>
+                        </Form.Group>
+                    </Form>
+                </div>
+                <div className="column">
+                    <Form>
+                        <Form.Group controlId="formStreet">
+                            <Form.Label>Street</Form.Label>
+                            <Form.Control type="text" value={street} onChange={(e) => setStreet(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group controlId="formCity">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control type="text" value={city} onChange={(e) => setCity(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group controlId="formState">
+                            <Form.Label>State</Form.Label>
+                            <Form.Control type="text" value={state} onChange={(e) => setState(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group controlId="formZip">
+                            <Form.Label>Zip</Form.Label>
+                            <Form.Control type="text" value={zip} onChange={(e) => setZip(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group controlId="formCountry">
+                            <Form.Label>Country</Form.Label>
+                            <Form.Control type="text" value={country} onChange={(e) => setCountry(e.target.value)} />
+                        </Form.Group>
+                    </Form>
+                </div>
             </div>
+            <div className="buttons-container">
+                <Button variant="primary" disabled={!changesMade} onClick={handleUpdateAccount}>Update Account</Button>
+                <Button variant="danger" onClick={handleDeleteAccount}>Delete Account</Button>
             </div>
         </div>
     );
-  }
 
 
+}
 
 export default Account;
