cd2e67a8a85b4b14b4c86a081ced93dbb5432e29
Cole Simpson <cole22simpson@gmail.com>
2023-04-27 19:35:56 -0700

    orders are now placed but it still looks ugly and needs a confirmation receipt

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
 import Axios from 'axios';
 import Form from "react-bootstrap/Form";
-import FlavorModal from "./FlavorModal";
-import TableRows from "./TableRows";
 import "./Orders.css"
 import Login from './Login';
 
@@ -18,76 +16,16 @@ const Orders = () => {
   const [shippingState, setShippingState] = useState("");
   const [shippingZip, setShippingZip] = useState("");
   const [shippingCountry, setShippingCountry] = useState("");
-  const [rowsData, setRowsData] = useState([]);
   const [checked, setChecked] = useState(true);
-  const [selectedFlavor, setSelectedFlavor] = useState("");
-  const [currentQuantity, setQuantity] = useState(0);
-  const [newOrder, setNewOrder] = useState([]);
+
+  const [flavors, setFlavors] = useState([]);
+  const [selectedFlavor, setSelectedFlavor] = useState('');
+  const [quantity, setQuantity] = useState(0);
+  const [orders, setOrders] = useState([]);
+  const [showShipping, setShowShipping] = useState(false);
+  const [reviewOrder, setReviewOrder] = useState(false);
 
   const loggedInUserID = localStorage.currentUserID;
-
-  var flavors = [
-    {
-        name: "Butter Pecan",
-        description: "Butter-flavored vanilla ice cream and chopped, toasted pecans.",
-        imageURL: "butter pecan"
-    },
-    {
-        name: "Chocolate Chip",
-        description: "Classic vanilla with chocolate chips.",
-        imageURL: "chocolate chip"
-    },
-    {
-        name: "Chocolate",
-        description: "Classic chocolate.",
-        imageURL: "chocolate"
-    },
-    {
-        name: "Mint Chocolate Chip",
-        description: "Cool and refreshing mint with chocolate chips mixed in.",
-        imageURL: "mint chocolate chip"
-    },
-    {
-        name: "Neopolitan",
-        description: "A blend of vanilla, chocolate, and strawberry.",
-        imageURL: "neopolitan"
-    },
-    {
-        name: "Rocky Road",
-        description: "Rich chocolate ice cream with nuts and marshmallows blended in.",
-        imageURL: "rocky road"
-    },
-    {
-        name: "Strawberry",
-        description: "Classic strawberry.",
-        imageURL: "strawberry"
-    },
-    {
-        name: "Ube",
-        description: "A sweet, nutty, and slightly floral taste.",
-        imageURL: "ube"
-    },
-    {
-        name: "Vanilla",
-        description: "Good old classic vanilla.",
-        imageURL: "vanilla"
-    }
-]
-
-  useEffect(() => {
-    Axios.get(`http://localhost:4000/users/getUser?id=${loggedInUserID}`).then((response) => {
-      setCurrentUser(response.data);
-      setLoading(false);
-    });
-  }, [setCurrentUser]);
-
-  if (isLoading) {
-    return (
-      <div className='orders'>
-        <h2 style={{ display: 'flex', justifyContent: 'center' }}>Please Log In</h2>
-      </div>
-    );
-  }
 
   const handleCheckBoxChange = () => {
     setChecked(!checked)
@@ -113,162 +51,328 @@ const Orders = () => {
     }
   }
 
-  const addTableRows = () => {
-    setSelectedFlavor("");
-    setQuantity(0);
-
-    const rowsInput = {
-      flavor: '',
-      quantity: ''
-    }
-    setRowsData([...rowsData, rowsInput])
-  }
-
-  const deleteTableRows = (index) => {
-    const rows = [...rowsData];
-    rows.splice(index, 1);
-    setRowsData(rows);
-  }
-
-  const handleTableSubmit = (index, evnt) => {
-    const {name, value} = evnt.target;
-    console.log(name);
-    const rowsInput = [...rowsData];
-    rowsInput[index][name] = value;
-    setRowsData(rowsInput);
-  }
-
-  const handleUpdateOrder = (newOrder) => {
-    console.log(newOrder);
-    setNewOrder(newOrder);
+  const handleGoToShipping = () => {
+    setShowShipping(true);
   };
 
-  return (
-    <div className='orders'>
-      {/* <div className='child'>
-        <p className='shippingaddressheader'>Shipping Address</p>
-        <Form>
-          <Form.Group size="lg" controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              autoFocus
-              type="email"
-              value={shippingEmail}
-              onChange={(e) => setShippingEmail(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group size="lg" controlId="firstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control
-              autoFocus
-              type="firstName"
-              value={shippingFirstName}
-              onChange={(e) => setShippingFirstName(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group size="lg" controlId="lastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              autoFocus
-              type="lastName"
-              value={shippingLastName}
-              onChange={(e) => setShippingLastName(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group size="lg" controlId="address">
-            <Form.Label>Address</Form.Label>
-            <Form.Control
-              autoFocus
-              type="address"
-              value={shippingAddress}
-              onChange={(e) => setShippingAddress(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group size="lg" controlId="city">
-            <Form.Label>City</Form.Label>
-            <Form.Control
-              autoFocus
-              type="city"
-              value={shippingCity}
-              onChange={(e) => setShippingCity(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group size="lg" controlId="state">
-            <Form.Label>State</Form.Label>
-            <Form.Control
-              autoFocus
-              type="state"
-              value={shippingState}
-              onChange={(e) => setShippingState(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group size="lg" controlId="zip">
-            <Form.Label>Address</Form.Label>
-            <Form.Control
-              autoFocus
-              type="zip"
-              value={shippingZip}
-              onChange={(e) => setShippingZip(e.target.value)}
-            />
-            <Form.Group size="lg" controlId="Country">
-              <Form.Label>Country</Form.Label>
+  const handleReviewOrder = (e) => {
+    e.preventDefault();
+    setReviewOrder(true);
+    setShowShipping(false);
+  };
+
+  const calculateShippingDate = () => {
+    const today = new Date();
+    const shippingDate = new Date(today.getTime() + 14 * 24 * 60 * 60 * 1000);
+    return shippingDate.toISOString().split('T')[0];
+  };
+
+  const addToOrder = () => {
+    if (quantity > 0 && selectedFlavor !== '') {
+      const shippingDate = new Date();
+      shippingDate.setDate(shippingDate.getDate() + 14);
+
+      const order = {
+        flavor: selectedFlavor,
+        quantity: quantity,
+        cost: 50,
+        shippingDate: shippingDate.toLocaleDateString(),
+      };
+
+      setOrders([...orders, order]);
+
+      // Reset form values
+      setSelectedFlavor('');
+      setQuantity(0);
+    }
+  };
+
+  console.log('orders:', orders);
+
+  const removeOrder = (index) => {
+    const updatedOrders = [...orders];
+    updatedOrders.splice(index, 1);
+    setOrders(updatedOrders);
+  };
+
+  const calculateShippingCost = () => {
+    const totalQuantity = orders.reduce((sum, order) => sum + order.quantity, 0);
+    const shippingCosts = Math.floor(totalQuantity / 10) * 5;
+    return shippingCosts;
+  };
+  
+  const calculateTotal = () => {
+    const totalCost = orders.reduce((sum, order) => sum + order.quantity * 30, 0);
+    const shippingCosts = calculateShippingCost();
+    const totalAmount = totalCost + shippingCosts;
+    return totalAmount;
+  };
+
+  const createOrder = async () => {
+    const order = {
+      customerID: loggedInUserID,
+      items: orders.map(order => ({
+        name: order.flavor,
+        quantity: order.quantity,
+        cost: order.quantity * 30
+      })),
+      shippingStatus: "Pending",
+      shippingDate: calculateShippingDate(),
+      shippingInfo: {
+        firstName: shippingFirstName,
+        lastName: shippingLastName,
+        street: shippingAddress,
+        city: shippingCity,
+        state: shippingState,
+        zip: shippingZip,
+        country: shippingCountry
+      }
+    };
+
+    try {
+      const response = await Axios.post("http://localhost:4000/orders/createOrder", order);
+      console.log("Order created:", response.data);
+    } catch (error) {
+      console.error("Error creating order:", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchFlavors = async () => {
+      try {
+        const response = await Axios.get('http://localhost:4000/flavors/getFlavors');
+        setFlavors(response.data.data);
+      } catch (error) {
+        console.error('Error fetching flavors:', error);
+      }
+    };
+  
+    fetchFlavors();
+  
+    Axios.get(`http://localhost:4000/users/getUser?id=${loggedInUserID}`).then((response) => {
+      setCurrentUser(response.data);
+      setLoading(false);
+      // const calculatedDate = calculateShippingDate();
+      // setShippingDate(calculatedDate);
+    });
+  }, [loggedInUserID, setCurrentUser]);
+
+  if (isLoading) {
+    return (
+      <div className='orders'>
+        <h2 style={{ display: 'flex', justifyContent: 'center' }}>Please Log In</h2>
+      </div>
+    );
+  }
+
+  if (showShipping) {
+    return (
+      <div className='orders'>
+        <div className='child'>
+          <p className='shippingaddressheader'>Shipping Address</p>
+          <Form>
+            <Form.Group size="lg" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                autoFocus
+                type="email"
+                value={shippingEmail}
+                onChange={(e) => setShippingEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="firstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                autoFocus
+                type="firstName"
+                value={shippingFirstName}
+                onChange={(e) => setShippingFirstName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="lastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                autoFocus
+                type="lastName"
+                value={shippingLastName}
+                onChange={(e) => setShippingLastName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="address">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                autoFocus
+                type="address"
+                value={shippingAddress}
+                onChange={(e) => setShippingAddress(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="city">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                autoFocus
+                type="city"
+                value={shippingCity}
+                onChange={(e) => setShippingCity(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="state">
+              <Form.Label>State</Form.Label>
               <Form.Control
                 autoFocus
                 type="state"
-                value={shippingCountry}
-                onChange={(e) => setShippingCountry(e.target.value)}
+                value={shippingState}
+                onChange={(e) => setShippingState(e.target.value)}
               />
             </Form.Group>
-          </Form.Group>
-          <Form.Check
-            type='switch'
-            id='custome-switch'
-            label='Use Default Information'
-            onChange={handleCheckBoxChange}
-          />
-        </Form>
-        <div className="button">
-          <button type="submit" className="btn btn-primary">Checkout</button>
-        </div>
-      </div> */}
-
-      <div className='child'>
-        <p className="orderformheader">Order Form</p>
-        <div className="row">
-          <div className="col-2"></div>
-            <div className="col-8">
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th>Flavor</th>
-                    <th>Quantity</th>
-                    <th><button className="btn btn-outline-success" onClick={addTableRows} >+</button></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <div className="orders-container">
-                  {newOrder.map((item, index) => (
-    <div key={index}>
-      <h4>{item.flavor}</h4>
-      <p>{item.quantity}</p>
-    </div>
-  ))}
-                  </div>
-                  <TableRows 
-                    handleUpdateOrder={handleUpdateOrder}
-                    flavors={flavors}
-                    rowsData={rowsData}
-                    deleteTableRows={deleteTableRows}
-                    handleChange={handleTableSubmit}
-                  />
-                </tbody>
-              </table>
+            <Form.Group size="lg" controlId="zip">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                autoFocus
+                type="zip"
+                value={shippingZip}
+                onChange={(e) => setShippingZip(e.target.value)}
+              />
+              <Form.Group size="lg" controlId="Country">
+                <Form.Label>Country</Form.Label>
+                <Form.Control
+                  autoFocus
+                  type="state"
+                  value={shippingCountry}
+                  onChange={(e) => setShippingCountry(e.target.value)}
+                />
+              </Form.Group>
+            </Form.Group>
+            <Form.Check
+              type='switch'
+              id='custome-switch'
+              label='Use Default Information'
+              onChange={handleCheckBoxChange}
+            />
+            <div className="button">
+              <button className="btn btn-primary" onClick={handleReviewOrder}>Review Order</button>
             </div>
-          <div className="col-2"></div>
+          </Form>
+          <div className="button">
+          <button className="btn btn-secondary" onClick={() => setShowShipping(false)}>Go Back</button>
+        </div>
         </div>
       </div>
-    </div>
+    );
+  }
 
+  if (reviewOrder) {
+    return (
+      <div className='orders'>
+        <div className='child'>
+          <div className='order-summary'>
+            <h3>Orders:</h3>
+            <ul>
+              {orders.map((order, index) => (
+                <li key={index}>
+                  <p>Flavor: {order.flavor}</p>
+                  <p>Quantity: {order.quantity}</p>
+                  <p>Cost: ${order.quantity * 30}</p>
+                  <p>Shipping Date: {order.shippingDate}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='shipping-details'>
+            <h3>Shipping Details:</h3>
+            <p>Email: {shippingEmail}</p>
+            <p>First Name: {shippingFirstName}</p>
+            <p>Last Name: {shippingLastName}</p>
+            <p>Address: {shippingAddress}</p>
+            <p>City: {shippingCity}</p>
+            <p>State: {shippingState}</p>
+            <p>Zip: {shippingZip}</p>
+            <p>Country: {shippingCountry}</p>
+            <p>Total: ${calculateTotal()}</p>
+            <p>Estimated Shipping Date: {calculateShippingDate()}</p>
+            <div className="button">
+              <button type="submit" className="btn btn-primary" onClick={createOrder}>Place Order</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+  return (
+    <div>
+      <h2>Ice Cream Order Form</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Flavor</th>
+            <th>Quantity</th>
+            <th>Cost</th>
+            <th>Est. Shipping Date</th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr className="order-selection">
+          <td>
+            <select value={selectedFlavor} onChange={(e) => setSelectedFlavor(e.target.value)}>
+              <option value="">Select a flavor</option>
+              {flavors.map((flavor) => (
+                <option key={flavor.id} value={flavor.name}>
+                  {flavor.name}
+                </option>
+              ))}
+            </select>
+          </td>
+          <td>
+            <input
+              type="number"
+              placeholder="0"
+              min={0}
+              value={quantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+            />
+          </td>
+          <td className="auto-cost">${quantity * 30}</td>
+          <td className="auto-date"></td>
+          <td>
+            <button onClick={addToOrder}>Add to Order</button>
+          </td>
+        </tr>
+        {orders.map((order, index) => (
+          <tr key={index}>
+            <td>
+              <p>{order.flavor}</p>
+            </td>
+            <td>
+              <p>{order.quantity}</p>
+            </td>
+            <td>
+              <p>${order.quantity * 30}</p>
+            </td>
+            <td>
+              <p>{order.shippingDate}</p>
+            </td>
+            <td>
+              <button onClick={() => removeOrder(index)}>Remove</button>
+            </td>
+          </tr>
+        ))}
+
+        <tr>
+          <td colSpan="2">Shipping Costs:</td>
+          <td colSpan="2">${calculateShippingCost()}</td>
+        </tr>
+        <tr>
+          <td colSpan="2">Total:</td>
+          <td colSpan="2">${calculateTotal()}</td>
+        </tr>
+        </tbody>
+      </table>
+      <div>
+        <button className="btn btn-primary" onClick={handleGoToShipping}>Go to Shipping Details</button>
+      </div>
+    </div>
   );
 }
 
--- src/components/TableRows.js
+++ src/components/TableRows.js
@@ -1,86 +1,181 @@
-import React, { useState, useEffect } from 'react';
-import FlavorModal from "./FlavorModal";
+// import React, { useState, useEffect } from 'react';
+// import FlavorDropdown from "./FlavorDropdown";
+// import axios from 'axios';
 
-const TableRows = ({ rowsData, flavors, handleUpdateOrder, deleteTableRows, handleChange }) => {
+// const TableRows = ({ rowsData, handleUpdateOrder, deleteTableRows, handleChange }) => {
+
+//     const [selectedFlavor, setSelectedFlavor] = useState("");
+//     const [currentQuantity, setQuantity] = useState(0);
+//     const [order, setOrder] = useState([]);
+//     const [flavors, setFlavors] = useState([]);
+
+//     const handleAddToOrder = () => {
+//         if (selectedFlavor && currentQuantity > 0) {
+//             const newOrder = [...order, {flavor: selectedFlavor, quantity: currentQuantity}];
+//             setOrder(newOrder);
+//             setSelectedFlavor("");
+//             setQuantity(0);
+//             handleUpdateOrder(newOrder);
+//         } else {
+//             window.alert("Please select a flavor and a number of units.")
+//         }
+//     }
     
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedFlavor, setSelectedFlavor] = useState("");
-    const [currentQuantity, setQuantity] = useState(0);
-    const [order, setOrder] = useState([]);
+//     const handleDeleteFromOrder = (index) => {
+//         const newOrder = [...order];
+//         newOrder.splice(index, 1);
+//         setOrder(newOrder);
+//         handleUpdateOrder(newOrder);
+//     }
 
-    const handleFlavorChoice = (flavor) => {
-        setSelectedFlavor(flavor);
-    }
+//     const handleSelectChange = (event) => {
+//         setSelectedFlavor(event.target.value);
+//     };
 
-    const toggleModal = () => {
-        setIsModalOpen(!isModalOpen);
-    }
+//     const handleQuantityChange = (event) => {
+//         const newQuantity = parseInt(event.target.value);
+//         setQuantity(newQuantity)
+//     }
 
-    const increaseQuantity = () => {
-        setQuantity(currentQuantity + 1);
-    }
-
-    const decreaseQuantity = () => {
-        if (currentQuantity > 0) {
-            setQuantity(currentQuantity - 1);
-        }
-    }
-
-    const handleAddToOrder = () => {
-        const newOrder = [...order, {flavor: selectedFlavor, quantity: currentQuantity}];
-        setOrder(newOrder);
-        setSelectedFlavor("");
-        setQuantity(0);
-        handleUpdateOrder(newOrder);
-    }
+//     useEffect(() => {
+//         const fetchFlavors = async () => {
+//           try {
+//             const response = await axios.get('http://localhost:4000/flavors/getFlavors');
+//             setFlavors(response.data.data);
+//           } catch (error) {
+//             console.error('Error fetching flavors:', error);
+//           }
+//         };
     
-    const handleDeleteFromOrder = (index) => {
-        const newOrder = [...order];
-        newOrder.splice(index, 1);
-        setOrder(newOrder);
-        handleUpdateOrder(newOrder);
-    }
-
-    useEffect(() => {
-        console.log(order);
-    }, [order]);
+//         fetchFlavors();
+//       }, []);
     
-    return (
-        rowsData.map((index) => {
-            return (
-                <tr key={index}>
-                    <td>
-                        {isModalOpen && (
-                        <FlavorModal
-                        flavors={flavors}
-                        onClose={toggleModal}
-                        chooseFlavor={handleFlavorChoice}  />
-                        )}
-                        <input type="text"
-                        value={selectedFlavor}
-                        onClick={() => toggleModal()}
-                        placeholder='Pick your favorite!'
-                        name="flavor" className="form-control flav-submission" />
-                    </td>
-                    <td>
-                        <button onClick={() => decreaseQuantity()} className="btn btn-secondary">
-                            -
-                        </button>
-                        <p className="quantity">
-                            {currentQuantity} {/* Quantity of ice creams in cart */}
-                        </p>
-                        <button onClick={() => increaseQuantity()} className="btn btn-secondary">
-                            +
-                        </button>
-                    </td>
-                    <button className="btn btn-outline-danger" onClick={() => {(handleDeleteFromOrder(index))}}>x</button>
-                    <button className="btn btn-outline-primary" onClick={() => handleAddToOrder(selectedFlavor, currentQuantity)}>Add to Order</button>
-                </tr>
-            )
-        })
+//     return (
+//         rowsData.map((data, index) => {
+//             const { flavor, quantity} = data;
+//             return (
+//                 <tr key={index}>
+//                     <td>
+//                         <div>
+//                         <FlavorDropdown
+//                         flavors={flavors}
+//                         handleSelectChange={handleSelectChange}
+//                         handleAddToOrder={handleAddToOrder}
+//                         />
+//                         </div>
+//                     </td>
+//                     <td>
+//                         <input
+//                             type="number"
+//                             placeholder="Enter number of units"
+//                             name="quantity"
+//                             className="form-control"
+//                             onChange={handleQuantityChange}
+//                         />
+//                     </td>
+//                     <td>
+//                     {/* <button className="btn btn-outline-danger" onClick={() => {(handleDeleteRow(index))}}>x</button> */}
+//                     </td>
+//                     <button className="btn btn-outline-primary" onClick={() => handleAddToOrder(selectedFlavor, currentQuantity)}>Add to Order</button>
+//                 </tr>
+//             )
+//         })
 
-    )
+//     )
 
-}
+// }
 
-export default TableRows;
+// export default TableRows;
+
+
+
+
+  // return (
+  //   <div className='orders'>
+  //     <div className='child'>
+  //       <p className='shippingaddressheader'>Shipping Address</p>
+  //       <Form>
+  //         <Form.Group size="lg" controlId="email">
+  //           <Form.Label>Email</Form.Label>
+  //           <Form.Control
+  //             autoFocus
+  //             type="email"
+  //             value={shippingEmail}
+  //             onChange={(e) => setShippingEmail(e.target.value)}
+  //           />
+  //         </Form.Group>
+  //         <Form.Group size="lg" controlId="firstName">
+  //           <Form.Label>First Name</Form.Label>
+  //           <Form.Control
+  //             autoFocus
+  //             type="firstName"
+  //             value={shippingFirstName}
+  //             onChange={(e) => setShippingFirstName(e.target.value)}
+  //           />
+  //         </Form.Group>
+  //         <Form.Group size="lg" controlId="lastName">
+  //           <Form.Label>Last Name</Form.Label>
+  //           <Form.Control
+  //             autoFocus
+  //             type="lastName"
+  //             value={shippingLastName}
+  //             onChange={(e) => setShippingLastName(e.target.value)}
+  //           />
+  //         </Form.Group>
+  //         <Form.Group size="lg" controlId="address">
+  //           <Form.Label>Address</Form.Label>
+  //           <Form.Control
+  //             autoFocus
+  //             type="address"
+  //             value={shippingAddress}
+  //             onChange={(e) => setShippingAddress(e.target.value)}
+  //           />
+  //         </Form.Group>
+  //         <Form.Group size="lg" controlId="city">
+  //           <Form.Label>City</Form.Label>
+  //           <Form.Control
+  //             autoFocus
+  //             type="city"
+  //             value={shippingCity}
+  //             onChange={(e) => setShippingCity(e.target.value)}
+  //           />
+  //         </Form.Group>
+  //         <Form.Group size="lg" controlId="state">
+  //           <Form.Label>State</Form.Label>
+  //           <Form.Control
+  //             autoFocus
+  //             type="state"
+  //             value={shippingState}
+  //             onChange={(e) => setShippingState(e.target.value)}
+  //           />
+  //         </Form.Group>
+  //         <Form.Group size="lg" controlId="zip">
+  //           <Form.Label>Address</Form.Label>
+  //           <Form.Control
+  //             autoFocus
+  //             type="zip"
+  //             value={shippingZip}
+  //             onChange={(e) => setShippingZip(e.target.value)}
+  //           />
+  //           <Form.Group size="lg" controlId="Country">
+  //             <Form.Label>Country</Form.Label>
+  //             <Form.Control
+  //               autoFocus
+  //               type="state"
+  //               value={shippingCountry}
+  //               onChange={(e) => setShippingCountry(e.target.value)}
+  //             />
+  //           </Form.Group>
+  //         </Form.Group>
+  //         <Form.Check
+  //           type='switch'
+  //           id='custome-switch'
+  //           label='Use Default Information'
+  //           onChange={handleCheckBoxChange}
+  //         />
+  //       </Form>
+  //       <div className="button">
+  //         <button type="submit" className="btn btn-primary">Checkout</button>
+  //       </div>
+  //     </div>
+  // );
